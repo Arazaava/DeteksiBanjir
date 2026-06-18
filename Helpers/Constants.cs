362e3d7536dd3065ef12276330f5c8e3e248d9be
Arazaava <arinaabaruu@gmail.com>
2026-06-18 23:14:32 +0700

    Add files via upload

---
+++ Helpers/Constants.cs
@@ -0,0 +1,7 @@
+namespace DeteksiBanjir.Helpers;
+
+public static class Constants
+{
+    // TODO: ISI API KEY GEMINI DI SINI
+    public const string GEMINI_API_KEY = "<<<MASUKKAN_API_KEY_GEMINI_DISINI>>>";
+}
